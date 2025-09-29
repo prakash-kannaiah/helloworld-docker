3545ad43c520dbcc53efa9198e8e1958fb0e2069
Prakash Kannaiah <105844116+prakash-kannaiah@users.noreply.github.com>
2025-09-29 15:01:15 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,8 +1,12 @@
 # app/app.py
 
-import time
+from flask import Flask
 
-while True:
-    print("Hello from Docker!")
-    time.sleep(5)  # print every 5 seconds
+app = Flask(__name__)
 
+@app.route("/")
+def hello():
+    return "<h1>Hello from Docker!</h1>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
