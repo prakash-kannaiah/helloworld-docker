dabc02f4cdc95cffd5326277180397b2bbc743f8
Prakash Kannaiah <105844116+prakash-kannaiah@users.noreply.github.com>
2025-10-04 09:31:54 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Hello from Docker Lite.v1.6!</h1>"
+    return "<h1>Hello from Docker Lite.v1.7!</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
